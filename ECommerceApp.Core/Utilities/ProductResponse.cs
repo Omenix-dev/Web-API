54660a1a231fa541c4b840ee87d3b38572f03eb6
Onyinyechi Onyenaucheya <preciousonyenaucheya10@gmail.com>
2022-09-06 02:53:21 +0100

    Added Repository

---
+++ ECommerceApp.Core/Utilities/ProductResponse.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Core.Utilities
+{
+    public class ProductResponse
+    {
+        public IQueryable<Product> Products{get; set; }
+        public int Pages { get; set; }  
+        public int CurrentPage { get; set; }
+    }
+}
---
+++ ECommerceApp.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using ECommerceApp.Core.Interface;
+using ECommerceApp.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ECommerceDbContext context;
+        public Repository(ECommerceDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRangeAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IQueryable<T>> GetAllAsync()
+        {
+            return (IQueryable<T>)await context.Set<T>().ToListAsync();
+            
+        }
+
+        public Task<T> GetAsync(System.Linq.Expressions.Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertRangeAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
