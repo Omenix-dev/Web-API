a8c51fee40b1147314609df42fb0b6c2da5df6fa
promise206 <okpalaugo.chukwuka1@gmail.com>
2022-09-11 23:45:04 +0100

    add UserId to registration in services

--- ECommerceApp.Core/Utilities/ECommerceProfile.cs
+++ ECommerceApp.Core/Utilities/ECommerceProfile.cs
@@ -17,6 +17,7 @@ namespace ECommerceApp.Core.Utilities
             CreateMap<OrderDetail, OrderDetailsDTO>().ReverseMap();
             CreateMap<Product, ProductDTO>().ReverseMap();
             CreateMap<User, RegistrationDTO>().ReverseMap()
+                .ForMember(dest=>dest.Id,act=>act.Equals(Guid.NewGuid().ToString()))
                 .ForMember(dest => dest.Email,act=>act.MapFrom(src=>src.Email.ToLower()))
                 .ForMember(dest=> dest.PasswordHash,act=>act.MapFrom(src=>SaltHashAlgorithm.GenerateHash(src.Password,src.PasswordSalt)));
         }
