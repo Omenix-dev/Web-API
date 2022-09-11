d884dec70c2879707ce4a6376eb3692c2d7f377e
promise206 <okpalaugo.chukwuka1@gmail.com>
2022-09-11 21:33:35 +0100

    Add Interface for Services

---
+++ ECommerceApp.Core/Interface/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using ECommerceApp.Core.DTO;
+using ECommerceApp.Domain.Model;
+
+namespace ECommerceApp.Core.Interface
+{
+    public interface IAuthService
+    {
+        Task<User> LoginAsync(LoginDTO details);
+        Task<bool> RegisterAsync(RegistrationDTO userDetails);
+    }
+}
